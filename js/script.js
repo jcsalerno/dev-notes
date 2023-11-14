83f7f87f6e27891003194466abab1c7cf5e4917f
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-14 18:16:18 -0300

    inclusão da funcionalidade para baixar em .CSV

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#export-notes");
 
 // Funções
 function showNotes() {
@@ -188,6 +189,29 @@ function searchNotes(search) {
   showNotes();
 }
 
+function exportData() {
+  const notes = getNotes();
+
+  // separa o dado por vírgula e ele quebra linha \n
+
+  const csvString = [
+    ["ID", "Conteúdo", "Fixado"],
+    ...notes.map((note) => [note.id, note.content, note.fixed]),
+  ]
+    .map((e) => e.join(","))
+    .join("\n");
+
+  const element = document.createElement("a");
+
+  element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+
+  element.target = "_blank";
+
+  element.download = "notes.csv";
+
+  element.click();
+}
+
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote());
 
@@ -201,6 +225,10 @@ noteInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     addNote();
   }
+});
+
+exportBtn.addEventListener("click", () => {
+  exportData();
 });
 
 // Inicialização
