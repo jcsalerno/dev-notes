9984e057b15fb4dd5925e016a7145a6a1d27b664
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-14 16:51:37 -0300

    adição da funcionalidade atualizando notas

--- js/script.js
+++ js/script.js
@@ -78,6 +78,11 @@ function createNote(id, content, fixed) {
   }
 
   // Eventos do elemento
+  element.querySelector("textarea").addEventListener("keyup", (e) => {
+    const noteContent = e.target.value;
+
+    updateNote(id, noteContent);
+  });
 
   element.querySelector(".bi-pin").addEventListener("click", () => {
     toggleFixNote(id);
@@ -136,6 +141,16 @@ function copyNote(id) {
   notesContainer.appendChild(noteElement);
 
   notes.push(noteObject);
+
+  saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  targetNote.content = newContent;
 
   saveNotes(notes);
 }
