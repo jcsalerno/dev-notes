60a6ce44a0956cf083a2e4ceca4881f043e8b17c
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-14 11:19:20 -0300

    salvando as notas no local storage.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 
 // Funções
+function showNotes() {
+  getNotes().forEach((note) => {
+    const noteElement = createNote(note.id, note.content, note.fixex);
+
+    notesContainer.appendChild(noteElement);
+  });
+}
+
 function addNote() {
+  const notes = getNotes();
+
   const noteObject = {
     id: generateId(),
     content: noteInput.value,
@@ -14,6 +24,12 @@ function addNote() {
   const noteElement = createNote(noteObject.id, noteObject.content);
 
   notesContainer.appendChild(noteElement);
+
+  notes.push(noteObject);
+
+  saveNotes(notes);
+
+  noteInput.value = "";
 }
 
 function generateId() {
@@ -36,5 +52,18 @@ function createNote(id, content, fixed) {
   return element;
 }
 
+// Local Storage
+function getNotes() {
+  const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+  return notes;
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote());
+
+// Inicialização
+showNotes();
