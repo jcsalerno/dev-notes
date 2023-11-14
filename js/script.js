acea3a6e9c932f8d8c83488c4bf107c8832eab3e
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-14 11:37:02 -0300

    fixando as notas

--- js/script.js
+++ js/script.js
@@ -49,7 +49,33 @@ function createNote(id, content, fixed) {
 
   element.appendChild(textarea);
 
+  const pinIcon = document.createElement("i");
+
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+
+  element.appendChild(pinIcon);
+
+  if (fixed) {
+    element.classList.add("fixed");
+  }
+
+  // Eventos do elemento
+
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
+
   return element;
+}
+
+function toggleFixNote(id) {
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  targetNote.fixed = !targetNote.fixed;
+
+  saveNotes(notes);
 }
 
 // Local Storage
